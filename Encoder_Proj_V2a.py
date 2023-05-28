6320ced38563ab1ef788869d0f5024a452c12880
davidblattdata <130545189+davidblattdata@users.noreply.github.com>
2023-05-28 16:41:08 -0700

    Add files via upload

--- Encoder_Proj_V2a.py
+++ Encoder_Proj_V2a.py
@@ -9,14 +9,11 @@ import sys
 
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71]
-print(len(letters))
-print(len(numbers))
+
 letter_to_num_dict = {key:value for key, value in zip(letters, numbers)}
 num_to_letter_dict = {key:value for key, value in zip(numbers, letters)}
     
-alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-    
-    #Optimize
+#Returns a new string based on a message and a keyword.
 def vigenere_encoder(message, keyword):
     count = 0
     new_string = ""
@@ -33,6 +30,7 @@ def vigenere_encoder(message, keyword):
         count += 1
     return new_string
 
+#Reverses the action of the vigenere encoder function.
 def vigenere_decoder(message, keyword):
     count = 0
     new_string = ""
@@ -48,10 +46,12 @@ def vigenere_decoder(message, keyword):
         count += 1
     return new_string
 
+#Exits the program.
 def exit_program():
     print('Program Ending...')
     sys.exit(0)
 
+#Returns a message.
 def input_message():
     while True:
         message = input('Enter a message: ')
@@ -66,6 +66,7 @@ def input_message():
         
     return message
 
+#Returns a keyword.
 def input_keyword():
     while True:
         keyword = input('Enter a keyword: ')
@@ -80,6 +81,7 @@ def input_keyword():
         
     return keyword
 
+#Allows user to enter a message and a keyword and returns an encoded string or returns a decoded string.
 def input_coder():
     print("\nWelcome to Vigenere Decoder!")
     while True:
@@ -97,5 +99,6 @@ def input_coder():
             message = input_message()
             keyword = input_keyword()
             print("\nYour decoded message is: " + str(vigenere_decoder(message, keyword) + "."))
-        
+
+#Starts the program
 input_coder()
